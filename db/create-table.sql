c8d0627770829a03b937bd2dfbf7d48d74f61890
Zahra <bghzahra@outlook.com>
2022-04-29 13:42:36 +0100

    added comments table to sql file

--- db/create-table.sql
+++ db/create-table.sql
@@ -4,3 +4,15 @@ CREATE TABLE pastes (
   	language TEXT,
   	code TEXT NOT NULL
 );
+
+
+DROP TABLE comments IF EXISTS
+
+CREATE TABLE comments (
+  pasteid INT, 
+  commentid SERIAL PRIMARY KEY, 
+  comment TEXT,
+    CONSTRAINT fk_pastes
+      FOREIGN KEY(pasteid) 
+	  REFERENCES pastes(id) ON DELETE CASCADE
+);
